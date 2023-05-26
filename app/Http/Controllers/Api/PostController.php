0f00b56aae102e11f2ad7f2302d72d0c7db1c49f
taniot <gaetano.frascolla@gmail.com>
2023-05-26 13:18:35 +0200

    feat: api - show post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -8,15 +8,36 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
-        $posts = Post::with('category', 'tags')->get();
+        //
         //$posts = Post::with('category', 'tags')->paginate(3);
 
+        //$posts = Post::all();
+        $posts = Post::with('category', 'tags')->get();
         return response()->json([
             'success' => true,
             'results' => $posts
         ]);
     }
-}
+    //public function show(Post $post) -> non ha category e tags
+    public function show(string $slug)
+    {
 
+        $post = Post::where('slug', $slug)->with('category', 'tags')->first();
+
+
+        if ($post) {
+            return response()->json([
+                'success' => true,
+                'results' => $post
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => null
+            ], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
 
 
 
